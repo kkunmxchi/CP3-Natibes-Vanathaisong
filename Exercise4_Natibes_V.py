43051202d53becb6b9c9020ab63920d2e8140fd3
Natibes Vanathaisong <94755659+kkunmxchi@users.noreply.github.com>
2021-11-21 21:33:29 +0700

    Add files via upload

---
+++ Exercise4_Natibes_V.py
@@ -0,0 +1,13 @@
+FE = 85
+GB = 82
+ICS = 90
+CP = 98
+
+print("-------------- Your Score---------------")
+print("")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
+print("")
+print("----------------------------------------")
