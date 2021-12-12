91f2e9a9332c1fcaf68beaaec83b9ad7f25f19b3
Natibes Vanathaisong <94755659+kkunmxchi@users.noreply.github.com>
2021-12-12 11:51:35 +0700

    Add files via upload

---
+++ Lecture50_Natibes_V.py
@@ -0,0 +1,15 @@
+def addNumber1(x,y):
+    print(x,"+",y,"=",x+y)
+def addNumber2(x,y):
+    print(x,"-",y,"=",x-y)
+def addNumber3(x,y):
+    print(x,"x",y,"=",x*y)
+def addNumber4(x,y):
+    print(x,"/",y,"=",int(x/y))
+
+x = int(input("Input Number x = "))
+y = int(input("Input Number y = "))
+addNumber1(x,y)
+addNumber2(x,y)
+addNumber3(x,y)
+addNumber4(x,y)
